577c987f2ec5bc8e4881f2c73e00355daa740b5c
Youssef Magdy <121168064+Magdy6565@users.noreply.github.com>
2024-10-10 23:53:22 +0300

    Update urls.py

--- webapp/my_app/urls.py
+++ webapp/my_app/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path('upload/', views.upload_video, name='upload_video'),
     path('detect-theft/', views.detect_theft, name='detect_theft'),    
-    path('', views.home, name='home'),  # Make sure you have a home view defined
-    # path('submitting', views.upload_video, name='upload_video'),  # Make sure you have a home view defined
+    path('', views.home, name='home'), 
 
 ]
